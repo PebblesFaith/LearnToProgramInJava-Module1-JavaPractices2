b54a859555b197740cde55be822712e887bf6cb7
Sarai H Ajai <ajai.sarai.h@outlook.com>
2019-10-30 17:07:06 -0500

    Create LiteralStringPractice1.java
    I have created, my LiteralStringPractice1.java file program in order for me to study for my edX.org online course "Learn to Program in Java" from a traditional "Building Java Programs" college textbook for Chapter 2 dated on October 29, 2019.

---
+++ LiteralStringPractice1.java
@@ -0,0 +1,11 @@
+package BookPracticesAssignments2;
+
+public class LiteralStringPractice1 {
+    public static void main(String[] args) {
+        System.out.println("hello" + "world!");
+        System.out.println("hello" .charAt(0));
+        System.out.println("hello" .length());
+        System.out.println("hello" .indexOf("e"));
+
+    }
+}
