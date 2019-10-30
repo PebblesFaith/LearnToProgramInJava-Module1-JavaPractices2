1d23fbf7e99e9c45ee589673755f12b4aa7edee2
Sarai H Ajai <ajai.sarai.h@outlook.com>
2019-10-30 17:49:17 -0500

    Create LiteralNumbersDataTypePractice1.java
    I have created, my LiteralNumbersDataTypesPractice1.java file program in order for me to study for my edX.org online course "Learn to Program in Java" from a traditional "Building Java Programs" college textbook for Chapter 2 dated on October 30, 2019.

---
+++ LiteralNumbersDataTypePractice1.java
@@ -0,0 +1,10 @@
+
+
+public class LiteralNumbersDataTypePractice1 {
+    public static void main(String[] args) {
+        System.out.println(3.0 + 6);
+        System.out.println(113 * 2.0);
+        System.out.println("1" + 2);
+        System.out.println(1 + 2 + "3" + 4 + 5);
+    }
+}
