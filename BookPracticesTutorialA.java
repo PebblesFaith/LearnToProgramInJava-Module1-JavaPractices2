7de5f24957ca4ac17912c7888f4009c93dc9e52a
Sarai H Ajai <ajai.sarai.h@outlook.com>
2019-10-29 19:14:32 -0500

    Create BookPracticesTutorialA.java
    I have created, my BookPracticesTutorialA.java file program in order for me to study for my edX.org online course "Learn to Program in Java" from traditional  "Building Java Programs" college textbook for Chapter 2 dated on October 29, 2019.

---
+++ BookPracticesTutorialA.java
@@ -0,0 +1,31 @@
+package BookPracticesAssignments2;
+
+public class BookPracticesTutorialA {
+    public static void main(String[] args) {
+
+        // write your code here
+        System.out.println(2 + 2);
+        System.out.println((3 + 29) - (4 * 5));
+        System.out.println(2.3e4);
+        System.out.println(119 / 5);
+        System.out.println(119 % 5);
+        System.out.println(1079 / 34);
+        System.out.println(1079 % 34);
+        System.out.print(19 / 4);
+        System.out.print(" remainder is ");
+        System.out.print(19 % 5);
+        System.out.println();
+        System.out.print("28 / 7  is = " + (28 / 7) + " and (28 % 7) is = " + (28 % 7));
+        System.out.println();
+        System.out.print(19 / 4);
+        System.out.print(" remainder is ");
+        System.out.print(19 % 5);
+        System.out.println();
+        System.out.println(10.2 % 2.4);
+        System.out.println(119.0 / 5.0);
+        System.out.println(19.0 / 5.0);
+        System.out.println(13 * 2 + 239 / 10 % 5 - 2 * 2);
+        System.out.println((int) 2.5 / .15);
+        System.out.println( (int) (2.5 / .15));
+    }
+}
