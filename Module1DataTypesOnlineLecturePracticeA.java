50fdca3c56064e34ce6f24d6891cc7d36333127d
Sarai H Ajai <ajai.sarai.h@outlook.com>
2019-10-29 19:38:34 -0500

    Create Module1DataTypesOnlineLecturePracticeA.java
    I have created, my Module1DataTypesOnlineLecturePracticeA.java file program in order for me to study for my edX.org online course "Learn to Program in Java" from a traditional  "Building Java Programs" college textbook for Chapter 2 dated on October 29, 2019.

---
+++ Module1DataTypesOnlineLecturePracticeA.java
@@ -0,0 +1,7 @@
+package BookPracticesAssignments2;
+
+public class Module1DataTypesOnlineLecturePracticeA {
+    public static void main(String[] args) {
+        System.out.println("hello" + "world!");
+    }
+}
