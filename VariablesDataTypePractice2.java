9a0518ff0fd7d60df9e8f91eb70636c7a5fd5529
Sarai H Ajai <ajai.sarai.h@outlook.com>
2019-10-31 18:43:34 -0500

    Create VariablesDataTypePractice2.java
    I have created, my VariableDataTypePractice2.java file program in order for me to study for my edX.org online course "Learn to Program in Java" from a traditional "Building Java Programs" college textbook for Chapter 2 dated on October 31, 2019.

---
+++ VariablesDataTypePractice2.java
@@ -0,0 +1,7 @@
+
+public class VariablesDataTypePractice2 {
+    public static void main(String[] args) {
+        Double gpa = 4.0;
+        System.out.print("Highest GPA: " + gpa + ".");
+    }
+}
