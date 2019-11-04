a2628b43d8c822882dd4c778cf160d384b912772
Sarai H Ajai <ajai.sarai.h@outlook.com>
2019-11-04 16:27:28 -0600

    Create TheForLoopPractice3.java
    I have created, my TheForLoopPractice3.java file program in order for me to study for my edX.org online course "Learn to Program in Java" from a traditional "Building Java Programs" college textbook for Chapter 2 dated on November 4, 2019.

---
+++ TheForLoopPractice3.java
@@ -0,0 +1,9 @@
+// A redundant program for squaring integers.
+public class TheForLoopPractice3 {
+    public static void main(String[] args) {
+        for (int i = -100; i <=100; i++) {
+            System.out.println(i + " squared = " + (i * i));
+        }
+    }
+
+}
