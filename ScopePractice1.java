2ff341f8ded2dd52bbef789f600363a2934a6e2b
Sarai H Ajai <ajai.sarai.h@outlook.com>
2019-11-04 18:53:57 -0600

    Create ScopePractice1.java
    I have created, my ScopePractice1.java file program in order for me to study for my edX.org online course "Learn to Program in Java" from a traditional "Building Java Programs" college textbook for Chapter 2 dated on November 1, 2019.

---
+++ ScopePractice1.java
@@ -0,0 +1,13 @@
+public class ScopePractice1 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            int squared = i * i;
+            System.out.println(i + " squared  = " + squared);
+        }
+        System.out.println();
+        for (int i = 1; i <= 5; i++) {
+            int squared = i * i;
+            System.out.println(i + " squared  = " + squared);
+        }
+    }
+}
